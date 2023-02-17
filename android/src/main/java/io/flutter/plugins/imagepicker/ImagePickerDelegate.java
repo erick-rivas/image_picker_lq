bb7200d375684f228baf2ce3eee7d5c5e000fe56
Erick Rivas <erick_rivas@outlook.com>
2023-02-17 17:12:35 -0600

    Include EXTRA_VIDEO_QUALITY=0 to video intent

--- android/src/main/java/io/flutter/plugins/imagepicker/ImagePickerDelegate.java
+++ android/src/main/java/io/flutter/plugins/imagepicker/ImagePickerDelegate.java
@@ -276,6 +276,7 @@ public class ImagePickerDelegate
 
   private void launchTakeVideoWithCameraIntent() {
     Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+    intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 0);
     if (this.methodCall != null && this.methodCall.argument("maxDuration") != null) {
       int maxSeconds = this.methodCall.argument("maxDuration");
       intent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, maxSeconds);
